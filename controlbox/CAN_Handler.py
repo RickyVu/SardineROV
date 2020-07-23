c354af8d4dc80ca9519da3b1da15fc2cde7a9a30
RickyVu <rickyvu189@gmail.com>
2020-07-23 16:23:18 +0800

    Pre-Water Test (23/07/2020 4:23)
    New:
    Thruster_Message fixed
    Physical and software system fully functional

--- controlbox/CAN_Handler.py
+++ controlbox/CAN_Handler.py
@@ -7,7 +7,7 @@ class CAN_Handler(Module):
     def __init__(self):
         self.bus = can.interface.Bus(bustype = "socketcan", channel = "can0", bitrate = 250000)
         pub.subscribe(self.MessageListener, 'Thruster Power Output')
-        pub.subscribe(self.InputListener, 'control-movement')
+        #pub.subscribe(self.InputListener, 'control-movement')
 
     def InputListener(self, message):
         pass
@@ -20,8 +20,8 @@ class CAN_Handler(Module):
                           is_extended_id=False)
 #        print(pub[0], pub[1], type(pub[0]))
         #print(pub[0])
-        if int(pub[0]) == 0x01D:
-            print("CAN Message", pub[0], "message", pub[1])
+        #if int(pub[0]) == 0x01D:
+        #    print("CAN Message", pub[0], "message", pub[1])
         try:
             #_ = 0
             self.bus.send(msg)
@@ -30,7 +30,23 @@ class CAN_Handler(Module):
             print("Message NOT sent")
     
     def run(self):
-        pass
-        #message = self.bus.recv(1)
+        message = self.bus.recv(1)
 #        print(message)
 
+
+if __name__ == '__main__':
+    handler = CAN_Handler()
+    handler.start(10)
+    import time
+    from random import randint
+    r = randint
+    for i in range(500):
+        pub.sendMessage('Thruster Power Output', pub=('0x017', r(4000, 6000)))
+        pub.sendMessage('Thruster Power Output', pub=('0x018', i * 20 + 1000))
+        pub.sendMessage('Thruster Power Output', pub=('0x01C', i * 20 + 1000))
+        time.sleep(0.3)
+
+    pub.sendMessage('Thruster Power Output', pub=('0x017', 0))
+    pub.sendMessage('Thruster Power Output', pub=('0x018', 0))
+    pub.sendMessage('Thruster Power Output', pub=('0x01C', 0))
+    print('stopped')
--- controlbox/Thruster_Message.py
+++ controlbox/Thruster_Message.py
@@ -13,16 +13,16 @@ class Thruster(Module):
         self.output_power = 0
 
     def run(self): #calculate rate, save in pub_data, then pub it
-        #rate = 1 * self.interval
-        rate = (self.target_power - self.current_power)*self.interval
-        if abs(rate)>self.max_rate:
-            rate = (rate/abs(rate)) * self.max_rate
-        self.current_power = self.current_power + rate
-        #difference = self.target_power - self.current_power
-        #if abs(difference)>rate:
-        #    self.current_power = self.current_power + difference/abs(difference)*rate
-        #else:
-        #    self.current_power = self.current_power + difference
+        rate = 0.5 * self.interval
+        #rate = (self.target_power - self.current_power)*self.interval
+        #if abs(rate)>self.max_rate:
+        #    rate = (rate/abs(rate)) * self.max_rate
+        #self.current_power = self.current_power + rate
+        difference = self.target_power - self.current_power
+        if abs(difference)>rate:
+            self.current_power = self.current_power + difference/abs(difference)*rate
+        else:
+            self.current_power = self.current_power + difference
         if abs(self.current_power)>1:
             self.output_power = self.current_power/abs(self.current_power)
         else:
@@ -38,12 +38,12 @@ class Thruster(Module):
             pub_data= (self.address, -1*self.output_power)
         else:
             pub_data= (self.address, self.output_power)
+
+        # pub_data = (self.address, int(self.target_power * 32767))
+
         #print(pub_data)
         pub.sendMessage('Thruster Power Output', pub = pub_data)
         pub.sendMessage(str(self.name)+'_Widget', output = pub_data[1]/32768)
-
-
-        
 
             
     def __listener(self, power): #Set target power
@@ -51,4 +51,13 @@ class Thruster(Module):
         #print(self.target_power)
 
 
-    
+if __name__ == '__main__':
+    import time
+    import CAN_Handler
+    can = CAN_Handler.CAN_Handler()
+    can.start(1)
+    tm = Thruster("ThrusterBL", "0x017", "False")
+    tm.start(20)
+    pub.sendMessage("ThrusterBL", power=0.4)
+    time.sleep(5)
+    pub.sendMessage("ThrusterBL", power=0)
