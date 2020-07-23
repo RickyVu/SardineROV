0e06bdaf81092da27d5bcb71647007d6442a709d
RickyVu <53203595+RickyVu@users.noreply.github.com>
2020-07-23 12:40:51 +0800

    Pre-Water Test (23/07/2020) Thruster_Message Fix?
    New
    Update to Thruster_Message, maybe fixed? Need further testing

--- controlbox/Thruster_Message.py
+++ controlbox/Thruster_Message.py
@@ -5,55 +5,50 @@ class Thruster(Module):
     def __init__(self, name, address, invert):
         self.name = name
         self.address = str(address)
-        self.invert = eval(invert)
+        self.invert = str(invert)
         pub.subscribe(self.__listener, name)
         self.target_power = 0
         self.current_power = 0
+        self.max_rate = 0.02
         self.output_power = 0
-        self.max_rate = 0.05
-    def run(self): #calculate rate, save in pub_data, then pub it
-        if self.invert:
-            self.current_power *= -1
-        difference = (self.target_power - self.current_power)*self.interval
-        #self.max_increment = 5 * self.interval
-        #difference = self.target_power - self.current_power
-        '''
-        if abs(difference) > self.max_increment:
-            self.current_power = self.current_power + (self.max_increment*difference/abs(difference))
-        else:
-            self.current_power = self.current_power + difference
-        '''
-        if abs(difference)> self.max_rate:
-            difference = (difference/abs(difference))*self.max_rate
-        self.current_power = self.current_power + difference
-        #if abs(self.current_power)>1:
-        #    self.current_power = self.current_power/abs(self.current_power)
 
+    def run(self): #calculate rate, save in pub_data, then pub it
+        #rate = 1 * self.interval
+        rate = (self.target_power - self.current_power)*self.interval
+        if abs(rate)>self.max_rate:
+            rate = (rate/abs(rate)) * self.max_rate
+        self.current_power = self.current_power + rate
+        #difference = self.target_power - self.current_power
+        #if abs(difference)>rate:
+        #    self.current_power = self.current_power + difference/abs(difference)*rate
+        #else:
+        #    self.current_power = self.current_power + difference
+        if abs(self.current_power)>1:
+            self.output_power = self.current_power/abs(self.current_power)
+        else:
+            self.output_power = self.current_power
 
         #Set to range of -32768 to 32767
         if self.output_power>=0:
-            self.output_power= int(self.current_power*32767)
+            self.output_power= int(self.output_power*32767)
         else:
-            self.output_power= int(self.current_power*32768)
+            self.output_power= int(self.output_power*32768)
 
-        pub_data= (self.address, self.output_power)
-        # Message Topic: Thruster/0a/Power
-        #if self.name == "ThrusterUB":
-        #    print(self.address, self.output_power)
+        if self.invert == "True":
+            pub_data= (self.address, -1*self.output_power)
+        else:
+            pub_data= (self.address, self.output_power)
+        #print(pub_data)
         pub.sendMessage('Thruster Power Output', pub = pub_data)
         pub.sendMessage(str(self.name)+'_Widget', output = pub_data[1]/32768)
-#        print(power)
-#        print(pub_data)
+
+
         
 
             
     def __listener(self, power): #Set target power
         self.target_power = power
-        #pub_data = (self.address, int(power*32767))
-        #pub.sendMessage("Thruster Power Output", pub = pub_data)
-        #pub.sendMessage(str(self.name)+"_Widget", output = int(pub_data[1]))
-        #print(pub_data)
-        #if self.name == "ThrusterUB":
-        #    print(self.target_power)
+        #print(self.target_power)
+
 
     
